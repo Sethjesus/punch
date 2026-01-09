487a46983d229ef8356771ecade4c4822f7943ba
Sethjesus <162777479+Sethjesus@users.noreply.github.com>
2026-01-09 10:06:54 +0800

    Update service worker cache version and files

--- sw.js
+++ sw.js
@@ -1,18 +1,23 @@
-const CACHE = "udm-hr-v1";
+const CACHE = "udm-hr-v2";
 
-self.addEventListener("install", e=>{
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(c=>
-      c.addAll([
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
         "./HR.html",
-        "./manifest.json"
+        "./index.html",
+        "./Leave.html",
+        "./manifest.webmanifest",
+        "./icon-192.png",
+        "./icon-512.png"
       ])
     )
   );
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch", e => {
   e.respondWith(
-    fetch(e.request).catch(()=>caches.match(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
